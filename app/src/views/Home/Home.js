3746e7963c6068cd4994b18de835f24767a79f5b
= <=>
2021-03-13 05:49:29 -0300

    Hotfix loading infinito

--- app/src/views/Home/Home.js
+++ app/src/views/Home/Home.js
@@ -1,35 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Container } from 'react-bootstrap'
 
-import { Stepper } from 'components'
+import { LoadingComponent, Stepper } from 'components'
+import { Form } from './Form'
 
 import useStepper from 'hooks/useStepper'
 import useTheme from 'hooks/useTheme'
 import useOrder from 'hooks/useOrderProvider'
-import { Form } from './Form'
 
 import styles from './styles'
 
 const Home = () => {
   const style = useTheme(styles)
-
+  const [loading, setLoading] = useState(true)
   const { activeStep } = useStepper()
-
   const { data, loadData } = useOrder()
 
   useEffect(() => {
     if (!data) {
-      loadData()
+      loadData(() => {
+        setLoading(false)
+      })
     }
-
     //eslint-disable-next-line
-  }, [activeStep, data])
+  }, [activeStep, data, loading])
 
   return (
     <Container fluid="xl" style={style.container}>
       <Stepper>
-        <Form />
+        <LoadingComponent loading={loading}>
+          <Form />
+        </LoadingComponent>
       </Stepper>
     </Container>
   )
